f1e0502030bdd1df99d0122d7c8bd6c4beb93ad7
Will Meyburgh <annekinmeyburgh@gmail.com>
2025-06-26 15:27:58 +0200

    stuff

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,17 +2,17 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  srcDir: '.', // Content source is the 'src' directory
+  srcDir: 'docs', // Content source is the 'src' directory
   title: "Annekin Meyburgh",
   description: "",
   appearance: 'force-dark',
   themeConfig: {
     // Set default to dark mode and remove the light/dark mode toggle
     nav: [
-      { text: 'Home', link: '/docs/' },
-      { text: 'About', link: '/docs/about' },
-      { text: 'Projects', link: '/docs/projects' },
-      { text: 'Contact', link: '/docs/contact' }
+      { text: 'Home', link: '/' },
+      { text: 'About', link: '/about' },
+      { text: 'Projects', link: '/projects' },
+      { text: 'Contact', link: '/contact' }
     ],
 
     socialLinks: [
