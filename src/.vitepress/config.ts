309e04ec013787d8d13875e32bda0f3ba6acb24b
Will Meyburgh <annekinmeyburgh@gmail.com>
2025-06-26 15:15:59 +0200

    Week 4, Day 4

--- src/.vitepress/config.ts
+++ src/.vitepress/config.ts
@@ -2,17 +2,17 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  srcDir: 'docs', // Content source is the 'docs' directory relative to the root
+  srcDir: '.', // Content source is the 'src' directory
   title: "Annekin Meyburgh",
   description: "",
   appearance: 'force-dark',
   themeConfig: {
     // Set default to dark mode and remove the light/dark mode toggle
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'About', link: '/about' }, // Adjusted link for new srcDir
-      { text: 'Projects', link: '/projects' }, // Adjusted link for new srcDir
-      { text: 'Contact', link: '/contact' } // Adjusted link for new srcDir
+      { text: 'Home', link: '/docs/' },
+      { text: 'About', link: '/docs/about' },
+      { text: 'Projects', link: '/docs/projects' },
+      { text: 'Contact', link: '/docs/contact' }
     ],
 
     socialLinks: [
